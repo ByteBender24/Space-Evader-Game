272e39ed4a9a66dd46947c8b68375e1e58757e0c
ByteBender24 <harishrajselva@gmail.com>
2024-01-25 23:51:13 +0530

    Window created'

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+import time
+
+WIDTH, HEIGHT = 1000, 800
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Evader")
+
+def main():
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
