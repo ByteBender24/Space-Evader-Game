24b4bb34353daa61d78447718ac9039d8d67e666
ByteBender24 <harishrajselva@gmail.com>
2024-01-26 10:48:35 +0530

    added background image

--- game.py
+++ game.py
@@ -2,10 +2,21 @@ import pygame
 import random
 import time
 
+# source C:/Users/HARI/.virtualenvs/Space_Evader-eCkaBzOt/Scripts/activate
+
 WIDTH, HEIGHT = 1000, 800
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Evader")
+
+BG = pygame.transform.scale(pygame.image.load(r"./assets/bg.jpeg"), (WIDTH, HEIGHT))
+#normal => BG = pygame.image.load(path, (coord as tuple))
+#ref docs for different scaling for background image
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
 
 def main():
 
@@ -17,7 +28,10 @@ def main():
                 run = False
                 break
 
+        draw()
+
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
