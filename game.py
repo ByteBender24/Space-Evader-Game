fa7ae500a2862b3ca9e8c8f5a6ddf172b0c519f8
ByteBender24 <harishrajselva@gmail.com>
2024-01-26 11:00:34 +0530

    added movement by keys arrows

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ BG = pygame.transform.scale(pygame.image.load(r"./assets/bg.jpeg"), (WIDTH, HEIG
 PLAYER_HEIGHT = 60
 PLAYER_WIDTH = 40
 
+PLAYER_VEL = 5
 
 def draw(player):
     WIN.blit(BG, (0, 0))
@@ -30,6 +31,7 @@ def main():
 
     player = pygame.Rect(WIDTH//2 - PLAYER_WIDTH, HEIGHT - PLAYER_HEIGHT,
                          PLAYER_WIDTH, PLAYER_HEIGHT)
+    
     # to center the player on x axis -> WIDTH//2 - PLAYER_WIDTH
     # to make the player on base -> HEIGHT - PLAYER_HEIGHT
     
@@ -38,9 +40,19 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT]:
+            player.x += PLAYER_VEL
+        if keys[pygame.K_UP]:
+            player.y -= PLAYER_VEL
+        if keys[pygame.K_DOWN]:
+                    player.y += PLAYER_VEL
         draw(player)
 
+        
     pygame.quit()
 
 
