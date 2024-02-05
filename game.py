e7f3c4792a3a8fd5c5094a4b401c1ea4b0a82dea
ByteBender24 <harishrajselva@gmail.com>
2024-02-05 07:41:08 +0530

    added stars, collision, losetext

--- game.py
+++ game.py
@@ -23,20 +23,66 @@ PLAYER_VEL = 5
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
+STAR_VEL = 3
 
-def draw(player, elapsed_time):
 
-    #window
+def draw(player, elapsed_time, stars):
+
+    # window
     WIN.blit(BG, (0, 0))
-    
-    #elapsed_time
-    time_text = FONT.render(f"Time : {round(elapsed_time)}s", 1, "white")   #1 ---> anti-aliasing
-    WIN.blit(time_text, (10,10))
 
-    #player-character
+    # elapsed_time
+    # 1 ---> anti-aliasing
+    time_text = FONT.render(f"Time : {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    for star in stars:
+        pygame.draw.rect(WIN, "white", star)
+    # player-character
     pygame.draw.rect(WIN, (255, 0, 0), player)  # also can use "red"
     pygame.display.update()
 
+
+def add_stars(stars, star_delay_high_time, star_low_time):
+    if star_delay_high_time > star_low_time:
+        num_stars_to_add = 3
+        new_stars = generate_stars(num_stars_to_add)
+        stars.extend(new_stars)
+
+        star_low_time = max(200, star_low_time - 50)
+        star_delay_high_time = 0
+
+    return star_low_time, star_delay_high_time
+
+
+def generate_stars(num_stars):
+    return [create_star() for _ in range(num_stars)]
+
+
+def create_star():
+    star_x = random.randint(0, WIDTH - STAR_WIDTH)
+    return pygame.Rect(star_x, 0-STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)  # 0-Star_Height for making it come from above screen
+
+
+def check_collisions(player, stars):
+    for star in stars[:]:
+        star.y += STAR_VEL
+        if star.y > HEIGHT:
+            stars.remove(star)
+        elif star.y + star.height >= player.y and star.colliderect(player):
+            stars.remove(star)
+            return True  # Player hit
+
+    return False
+
+
+def display_game_over():
+    lost_text = FONT.render("You Lost!", 1, "white")
+    WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width() /
+             2, HEIGHT/2 - lost_text.get_height()/2))
+    pygame.display.update()
 
 def main():
 
@@ -52,9 +98,22 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    star_low_time = 2000
+    star_delay_high_time = 0
+    stars = []
+    hit = False
+
     while run:
 
-        clock.tick(60)  # framerate = 60
+        star_delay_high_time += clock.tick(60)  # framerate = 60
+
+        star_low_time, star_delay_high_time = add_stars(
+            stars, star_delay_high_time, star_low_time)
+
+        if check_collisions(player, stars):
+            display_game_over()
+            pygame.time.delay(4000)
+            break
 
         elapsed_time = time.time() - start_time
 
@@ -78,7 +137,7 @@ def main():
         if keys[pygame.K_DOWN] and (player.y + PLAYER_VEL + player.height <= HEIGHT):
             player.y += PLAYER_VEL
 
-        draw(player, elapsed_time)
+        draw(player, elapsed_time, stars)
 
     pygame.quit()
 
